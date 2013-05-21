d4b48cd47089d721990df554c46b75434850d98a
Kevin Poorman <kjp@brightleafsoftware.com>
2013-05-21 15:29:36 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudspokes2263::Application.config.secret_token = 'b5e773eef6585ecc96a721264071d981568f004a393215c64a4fa116a173f3bbefcd70e4ccd426f9515bc9365490cd8fb462ea78fbd491f24da026c548174101'
